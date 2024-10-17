b8eb5d19a644078314e83d203d4edd6402312558
idriss96 <idrissatik1996@gmail.com>
2024-10-17 19:19:22 +0200

    add recipe list card

--- food-recipe-app/src/app/loading.js
+++ food-recipe-app/src/app/loading.js
@@ -1,7 +1,9 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
     return (
-        <div>
-            Loading...
+        <div className="w-full min-h-screen">
+           <Skeleton/>
         </div>
     );
 }
